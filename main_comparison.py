be69588d575208732a1e9b9b0b030d7a12731a74
mariano <mcabezas@eia.udg.edu>
2021-09-22 12:32:03 +1000

    redone the train / val split

--- main_comparison.py
+++ main_comparison.py
@@ -1,7 +1,8 @@
 import argparse
 import os
+import json
+import nibabel
 import numpy as np
-import nibabel as nib
 import time
 import datasets
 import models
@@ -9,8 +10,9 @@ import yaml
 import torch
 from torch.utils.data import DataLoader
 from time import strftime
-from utils import find_file, get_mask, get_normalised_image
+from utils import find_file, get_mask, get_normalised_image, get_bb
 from utils import color_codes, time_to_string
+from skimage.measure import label as bwlabeln
 
 
 """
@@ -89,8 +91,26 @@ def load_image_list(path, image_list, roi):
     return np.stack(images).astype(np.float16)
 
 
-def get_data(experiment_config, subject_list):
+def get_images(experiment_config, subject, session=None):
     d_path = experiment_config['path']
+    p_path = os.path.join(d_path, subject)
+    if session is not None:
+        p_path = os.path.join(p_path, session)
+    roi = get_mask(find_file(experiment_config['roi'], p_path))
+    label = get_mask(find_file(experiment_config['labels'], p_path))
+    if isinstance(experiment_config['files'], tuple):
+        images = tuple(
+            load_image_list(p_path, file_i, roi)
+            for file_i in experiment_config['files']
+        )
+    else:
+        images = load_image_list(
+            p_path, experiment_config['files'], roi
+        )
+    return roi, label, images
+
+
+def get_data(experiment_config, subject_list):
     load_start = time.time()
 
     subjects = []
@@ -101,7 +121,6 @@ def get_data(experiment_config, subject_list):
         load_elapsed = time.time() - load_start
         load_eta = loads * load_elapsed / (pi + 1)
         if experiment_config['multisession']:
-            p_path = os.path.join(d_path, p['subject'])
             sessions = p['sessions']
             for si, session in enumerate(sessions):
                 print(
@@ -113,24 +132,13 @@ def get_data(experiment_config, subject_list):
                         time_to_string(load_eta),
                     ), end='\r'
                 )
-                s_path = os.path.join(p_path, session)
-                roi = get_mask(find_file(experiment_config['roi'], s_path))
-                rois.append(roi)
-                labels.append(
-                    get_mask(find_file(experiment_config['labels'], s_path))
+                roi, label, images = get_images(
+                    experiment_config, p['subject'], session
                 )
-                if isinstance(experiment_config['files'], tuple):
-                    images = tuple(
-                        load_image_list(s_path, file_i, roi)
-                        for file_i in experiment_config['files']
-                    )
-                else:
-                    images = load_image_list(
-                        s_path, experiment_config['files'], roi
-                    )
+                rois.append(roi)
+                labels.append(label)
                 subjects.append(images)
         else:
-            p_path = os.path.join(d_path, p)
             print(
                 '\033[KLoading subject {:} ({:d}/{:d}) '
                 '{:} ETA {:}'.format(
@@ -139,20 +147,11 @@ def get_data(experiment_config, subject_list):
                     time_to_string(load_eta),
                 ), end='\r'
             )
-            roi = get_mask(find_file(experiment_config['roi'], p_path))
-            rois.append(roi)
-            labels.append(
-                get_mask(find_file(experiment_config['labels'], p_path))
+            roi, label, images = get_images(
+                experiment_config, p['subject']
             )
-            if isinstance(experiment_config['files'], tuple):
-                images = tuple(
-                    load_image_list(p_path, file_i, roi)
-                    for file_i in experiment_config['files']
-                )
-            else:
-                images = load_image_list(
-                    p_path, experiment_config['files'], roi
-                )
+            rois.append(roi)
+            labels.append(label)
             subjects.append(images)
     print('\033[K', end='\r')
     return subjects, labels, rois
@@ -247,14 +246,111 @@ def train(config, net, training, validation, model_name, verbose=0):
         net.save_model(os.path.join(path, model_name))
 
 
+def test_images(config, mask_name, net, subject, session=None):
+    masks_path = config['masks_path']
+    d_path = os.path.join(config['path'], subject)
+    p_path = os.path.join(masks_path, subject)
+    if not os.path.isdir(p_path):
+        os.mkdir(p_path)
+    if session is not None:
+        p_path = os.path.join(p_path, session)
+        d_path = os.path.join(d_path, session)
+        if not os.path.isdir(p_path):
+            os.mkdir(p_path)
+
+    roi, label, images = get_images(
+        config, subject, session
+    )
+    bb = get_bb(roi, 2)
+
+    prediction_file = find_file(mask_name, p_path)
+    if prediction_file is None:
+        prediction_file = os.path.join(p_path, mask_name)
+        segmentation = np.zeros_like(label)
+        none_slice = (slice(None, None),)
+        # Patch "extraction".
+        if isinstance(images, tuple):
+            data = tuple(
+                data_i[none_slice + bb].astype(np.float32)
+                for data_i in images
+            )
+        else:
+            data = images[none_slice + bb].astype(np.float32)
+        prediction = net.inference(data) > 0.5
+        segmentation[bb] = prediction
+        segmentation[np.logical_not(roi)] = 0
+        ref_nii = nibabel.load(os.path.join(d_path, config['labels']))
+        segmentation_nii = nibabel.Nifti1Image(
+            segmentation, ref_nii.get_qform(), ref_nii.header
+        )
+        segmentation_nii.to_filename(prediction_file)
+    else:
+        segmentation = nibabel.load(prediction_file).get_fdata()
+        prediction = segmentation[bb].astype(bool)
+
+    target = label[bb].astype(bool)
+    no_target = np.logical_not(target)
+    target_regions, gtr = bwlabeln(target, return_num=True)
+    no_prediction = np.logical_not(prediction)
+    prediction_regions, r = bwlabeln(prediction, return_num=True)
+    true_positive = np.sum(np.logical_and(target, prediction))
+    false_positive = np.sum(np.logical_and(no_target, prediction))
+
+    results = {
+        'TPV': true_positive,
+        'TNV': np.sum(np.logical_and(no_target, no_prediction)),
+        'FPV': false_positive,
+        'FNV': np.sum(np.logical_and(target, np.logical_not(prediction))),
+        'TPR': len(np.unique(target_regions[true_positive])),
+        'FPR': len(np.unique(prediction_regions[false_positive])),
+        'GTR': gtr,
+        'R': r
+    }
+    return results
+
+
 def test(config, seed, net, testing, training, validation=None, verbose=0):
     # Init
-    c = color_codes()
     options = parse_inputs()
-    masks_path = config['masks_path']
     mask_base = os.path.splitext(os.path.basename(options['config']))[0]
 
-    print(list(testing.keys()))
+    testing_subjects = list(testing.keys())
+    test_start = time.time()
+    mask_name = '{:}-test.s{:05d}.nii.gz'.format(mask_base, seed)
+    for sub_i, subject in enumerate(testing_subjects):
+        tests = len(testing_subjects) - sub_i
+        test_elapsed = time.time() - test_start
+        test_eta = tests * test_elapsed / (sub_i + 1)
+        if config['multisession']:
+            sessions = list(subject.keys())
+            for sess_j, session in enumerate(sessions):
+                if verbose:
+                    print(
+                        '\033[KTesting subject {:} [{:}]'
+                        ' ({:d}/{:d} - {:d}/{:d}) {:} ETA {:}'.format(
+                            subject, session, sub_i + 1, len(testing_subjects),
+                            sess_j + 1, len(subject['sessions']),
+                            time_to_string(test_elapsed),
+                            time_to_string(test_eta),
+                        ), end='\r'
+                    )
+                results = test_images(config, mask_name, net, subject, session)
+                for r_key, r_value in results.items():
+                    testing[subject][session][seed][r_key] = r_value
+        else:
+            if verbose > 0:
+                print(
+                    '\033[KTesting subject {:} ({:d}/{:d}) {:} ETA {:}'.format(
+                        subject, sub_i + 1, len(testing_subjects),
+                        time_to_string(test_elapsed),
+                        time_to_string(test_eta),
+                    ), end='\r'
+                )
+            results = test_images(config, mask_name, net, subject)
+            for r_key, r_value in results.items():
+                testing[subject][seed][r_key] = r_value
+
+    print(testing)
     print(training)
     if validation is not None:
         print(validation)
@@ -294,21 +390,46 @@ def main(verbose=2):
 
     # We want a common starting point
     subjects = get_subjects(config)
-    testing_results = {
-        subject: {
-            str(seed): {
-                'TPV': [],
-                'TNV': [],
-                'FPV': [],
-                'FNV': [],
-                'TPR': [],
-                'FPR': [],
-                'FNR': [],
+    if config['multisession']:
+        testing_results = {
+            subject: {
+                session: {
+                    str(seed): {
+                        'TPV': None,
+                        'TNV': None,
+                        'FPV': None,
+                        'FNV': None,
+                        'TPR': None,
+                        'FPR': None,
+                        'FNR': None,
+                        'GTR': None,
+                        'R': None,
+                    }
+                    for seed in seeds
+                }
             }
-            for seed in seeds
+            for t_list in subjects.values() for subject in t_list
+            for session in subject['sessions']
         }
-        for t_list in subjects.values() for subject in t_list
-    }
+    else:
+        testing_results = {
+            subject: {
+                str(seed): {
+                    'TPV': None,
+                    'TNV': None,
+                    'FPV': None,
+                    'FNV': None,
+                    'TPR': None,
+                    'FPR': None,
+                    'FNR': None,
+                    'GTR': None,
+                    'R': None,
+                }
+                for seed in seeds
+            }
+            for t_list in subjects.values() for subject in t_list
+        }
+
     if isinstance(config['files'], tuple):
         n_images = len(config['files'][0])
     else:
@@ -444,6 +565,18 @@ def main(verbose=2):
                     n_images=n_images
                 )
                 net.load_model(model_name)
+            json_name = '{:}-testing.f{:d}.s{:d}.jsom'.format(
+                model_base, i, seed
+            )
+            with open(os.path.join(masks_path, json_name), 'w') as testing_json:
+                json.dump(testing_results, testing_json)
+
+    json_name = '{:}-testing.jsom'.format(model_base)
+    with open(os.path.join(masks_path, json_name), 'w') as testing_json:
+        json.dump(testing_results, testing_json)
+
+    # Read data from file:
+    # data = json.load(open("file_name.json"))
 
 
 if __name__ == '__main__':
