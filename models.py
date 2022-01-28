a74d847536a9dd82787576e5a1e13f31adea983a
mariano <mcabezas@eia.udg.edu>
2022-01-28 12:31:15 +1100

    saved fisher to cpu

--- models.py
+++ models.py
@@ -243,21 +243,21 @@ class MetaModel(BaseModel):
             for n, p in self.model.named_parameters():
                 if p.requires_grad:
                     self.ewc_parameters[n]['fisher'].append(
-                        new_fisher[n]
+                        new_fisher[n].cpu()
                     )
                     self.ewc_parameters[n]['means'].append(
-                        p.data.detach()
+                        p.data.detach().cpu()
                     )
         else:
             for n, p in self.model.named_parameters():
                 if p.requires_grad:
-                    self.ewc_parameters[n]['means'] = p.data.detach()
+                    self.ewc_parameters[n]['means'] = p.data.detach().cpu()
                     if self.first:
-                        self.ewc_parameters[n]['fisher'] = new_fisher[n]
+                        self.ewc_parameters[n]['fisher'] = new_fisher[n].cpu()
                     else:
                         prev_fisher = self.ewc_parameters[n]['fisher']
                         fisher_t0 = (1 - self.ewc_alpha) * prev_fisher
-                        fisher_t1 = self.ewc_alpha * new_fisher[n]
+                        fisher_t1 = self.ewc_alpha * new_fisher[n].cpu()
                         self.ewc_parameters[n]['fisher'] = (
                             fisher_t0 + fisher_t1
                         )
