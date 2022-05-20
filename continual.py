acf2f14818b5bd10cba72913ebcc1534aebd9541
mariano <mcabezas@eia.udg.edu>
2022-05-20 10:07:35 +1000

    bug fixes and improvemeents

--- continual.py
+++ continual.py
@@ -376,6 +376,7 @@ class GEM(MetaModel):
     def update_memory(self, x, y):
         # Update ring buffer storing examples from current task
         t = self.current_task
+        print(t)
         bsz = y.data.size(0)
         endcnt = min(self.mem_cnt + bsz, self.n_memories)
         effbsz = endcnt - self.mem_cnt
