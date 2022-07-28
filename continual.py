7b546f15fc1c651f70284b9f38ae04e6d965c023
mcabezas <mcabezas@eia.udg.edu>
2022-07-28 12:18:03 +1000

    removed debugging

--- continual.py
+++ continual.py
@@ -825,6 +825,7 @@ class iCARL(MetaModel):
 
     def epoch_update(self, epochs, loader):
         if (self.epoch + 1) == epochs:
+            print('Updating memory')
             # Get labels from previous task; we assume labels are consecutive
             all_labs = torch.LongTensor(np.unique(self.memy.numpy()))
             num_classes = all_labs.size(0)
