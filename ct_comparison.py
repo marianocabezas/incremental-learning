b430c26ef58e2384ba144158186aed341b4b6468
mcabezas <mcabezas@eia.udg.edu>
2023-02-21 16:26:52 +1100

    fixed CTmodel

--- ct_comparison.py
+++ ct_comparison.py
@@ -231,6 +231,7 @@ def test(config, net, testing, task, n_classes, verbose=0):
             x.cpu().numpy(), nonbatched=False, task=task
         )
         target = y.cpu().numpy()
+        print(prediction, target)
 
         for pred_sub, target_sub in zip(prediction, target):
             for k in range(n_classes):
