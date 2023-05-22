c6239962a0f5df2f8cab2c428b681f5df19990bf
mcabezas <mcabezas@eia.udg.edu>
2023-05-22 15:11:00 +1000

    bug fix tmp

--- natural_random_comparison.py
+++ natural_random_comparison.py
@@ -414,6 +414,7 @@ def main(verbose=2):
 
     epochs = config['epochs']
     config['no_color'] = options['no_color']
+    config['tmp'] = options['tmp']
 
     try:
         pretrained = config['pretrained']
