e7c1bc4083e6c71f854b7ee075d12c97a96427ca
mcabezas <mcabezas@eia.udg.edu>
2023-06-27 11:22:59 +1000

    added option to not store model weights

--- natural_random_comparison.py
+++ natural_random_comparison.py
@@ -678,7 +678,7 @@ def main(verbose=2):
                 )
                 train(
                     config, seed, net, training_set,
-                    model_name, 1, n_tasks, 2, clean=options['clean']
+                    model_name, 1, n_tasks, 2
                 )
                 update_results(
                     config, net, seed, epoch, epochs, nc_per_task, 0,
