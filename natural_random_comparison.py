47f99f4a65146908eaa40dc8eb2468da5f786c9d
mcabezas <mcabezas@eia.udg.edu>
2023-09-28 13:44:26 +1000

    added ImageNet processing

--- natural_random_comparison.py
+++ natural_random_comparison.py
@@ -159,7 +159,7 @@ def split_imagenet(dataset, all_classes, classes):
         class_names = sorted(os.listdir(dataset.split_folder))
         task_classes = all_classes[i * classes:i * classes + classes]
         task_class_idx = [
-            idx for idx, _ in task_classes
+            tuple(idx) for idx, _ in task_classes
         ]
         d_task = deepcopy(dataset)
         d_task.classes = [cls for _, cls in task_classes]
