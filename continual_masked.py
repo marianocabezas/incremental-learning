1d7e57a88c0364d28a789f5e51f6b54f7a49db01
mcabezas <mcabezas@eia.udg.edu>
2023-11-23 12:33:23 +1100

    fixed der?

--- continual_masked.py
+++ continual_masked.py
@@ -814,12 +814,12 @@ class DER(IncrementalModelMemory):
         pred_y, x_cuda, y_cuda = BaseModel.observe(self, x, y)
         if self.current_task > 0:
             print(
-                'Pre update', y_cuda, pred_y.shape, x_cuda.shape, y_cuda.shape,
+                'Pre update', y_cuda, y_cuda.shape,
                 self.task_mask, self.task_masks, self.global_mask
             )
         y_cuda = update_y(y_cuda, self.global_mask)
         if self.current_task > 0:
-            print('Post update', y_cuda, pred_y.shape, x_cuda.shape, y_cuda.shape)
+            print('Post update', y_cuda, y_cuda.shape)
         return pred_y, x_cuda, y_cuda
 
     def forward(self, *inputs):
