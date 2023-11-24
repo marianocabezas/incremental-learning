d0889bd7236798c99ec1507379ed618d47f0bea6
mcabezas <mcabezas@eia.udg.edu>
2023-11-24 15:57:43 +1100

    fixed der?

--- continual_masked.py
+++ continual_masked.py
@@ -325,6 +325,7 @@ class IncrementalModelMemory(IncrementalModel):
         self._update_cum_grad(batches)
 
     def batch_update(self, batch, batches, x, y):
+        print('Batch update!', batch, batches)
         if self.task:
             y = self.task_mask[y]
         if self.memory_manager is not None:
