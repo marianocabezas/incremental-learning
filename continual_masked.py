7bf3e706abc8335e1f398fd686fb3788ab90e86b
mcabezas <mcabezas@eia.udg.edu>
2023-11-27 17:43:17 +1100

    fixed piggyback?

--- continual_masked.py
+++ continual_masked.py
@@ -1454,8 +1454,11 @@ class Piggyback(IncrementalModel):
             for i, (c_mask, n_mask, layer) in enumerate(zip(
                 self.current_mask, new_mask, self.model_layers
             )):
+                # Mask with all the "prunable weights" for the layer.
                 prune_mask = torch.logical_not(c_mask)
                 n_elem = torch.sum(prune_mask)
+                # Boolean list. True means the weights must be pruned, while
+                # False means not a prunable weight.
                 flat_mask = dropped_weights[mask_idx:mask_idx + n_elem]
                 # The mask represents the weights we want to keep secured.
                 # However, as the next step involves retraining the weights
@@ -1463,11 +1466,6 @@ class Piggyback(IncrementalModel):
 
                 # New mask will contain the fixed weights (previous + new).
                 n_mask.data[prune_mask].copy_(torch.logical_not(flat_mask))
-                n_mask.data[torch.logical_not(prune_mask)].fill_(True)
-                # Current mask will contain the pruned weights (we do not want
-                # to train them) plus the previous ones already stored.
-                c_mask.data[prune_mask].copy_(flat_mask)
-
                 layer.weight.data[n_mask].fill_(0.0)
                 print(
                     'Filling {:,}[:,]/{:,}[{:,}] weights'
@@ -1478,6 +1476,12 @@ class Piggyback(IncrementalModel):
                         mask_idx, mask_idx + n_elem
                     )
                 )
+                n_mask.data[torch.logical_not(prune_mask)].fill_(True)
+                # Current mask will contain the pruned weights (we do not want
+                # to train them) plus the previous ones already stored.
+                c_mask.data[prune_mask].copy_(flat_mask)
+                c_mask.data[torch.logical_not(prune_mask)].fill_(True)
+
                 mask_idx += n_elem
 
             # 3) Retrain the pruned network
