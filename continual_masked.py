65647ccaebf763216c8fe847d0cd218620c78c78
mcabezas <mcabezas@eia.udg.edu>
2023-11-29 17:58:06 +1100

    fixed piggyback?

--- continual_masked.py
+++ continual_masked.py
@@ -910,6 +910,7 @@ class DER(IncrementalModelMemory):
     ):
         # 1) Representation learning stage
         if task not in self.observed_tasks:
+            print('yeeeah', task, self.observed_tasks)
             self.current_task = task
             n_classes = len(task_mask)
             self.task_fc = nn.Linear(
